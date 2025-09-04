67a9804af10e1b0948e75e387bed9e7e23936b16
nada032 <jyoon2233@naver.com>
2025-09-04 17:23:18 +0900

    최종
    TREEFinal

---
+++ Assets/Prefabs/Scripts/player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    { 
+        if(Input.GetKey(Keycode))
+        
+    }
+}
